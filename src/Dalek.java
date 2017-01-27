3895f0ba969f5e88b0d4832f1cc53de2f0862162
Jacob <Jacob@192.168.0.19>
2017-01-27 00:06:16 -0500

    done assignment 4

--- src/Dalek.java
+++ src/Dalek.java
@@ -12,37 +12,89 @@ import java.awt.Color;
  */
 public class Dalek {
     private int row;
+
     private int col;
-    private Color colour;
-    
-    public Dalek(int row, int col){
-        //Set a random locatins for the Daleks
-        
-        //Dalek 1
-        int randCol1 = (int) (Math.random() * 12);
-        int randRow1 = (int) (Math.random() * 12);
-        Dalek kip1 = new Dalek(randCol1, randRow1);
-        
-        //Dalek 2
-        int randCol2 = (int) (Math.random() * 12);
-        int randRow2 = (int) (Math.random() * 12);
-        Dalek kip2 = new Dalek(randCol2, randRow2);
-        
-        //Dalek 3
-        int randCol3 = (int) (Math.random() * 12);
-        int randRow3 = (int) (Math.random() * 12);
-        Dalek kip3 = new Dalek(randCol3, randRow3);
+
+    private boolean dCrash = false;
+
+    /**
+     * class for Daleks row and col
+     *
+     * @param row random row
+     * @param col random col
+     */
+    public Dalek(int row, int col) {
+        this.row = row;
+        this.col = col;
+
     }
-    
-    public void move(int newRow, int newCol){
+
+    /**
+     * move towards the doctor method
+     *
+     * @param doc the doctor that Daleks are moving toward
+     */
+    public void moveTowards(Doctor doc) {
+        //if dalek has not crashed
+        int docRow = doc.getRow();
+        int docCol = doc.getCol();
         
+        //if daleks row is more than doctors row, -1 from daleks row 
+        if (this.row > docRow) {
+            this.row -= 1;
+
+        //if daleks row  is less  than doctors col, +1 to daleks row
+        } else if (this.row < docRow) {
+            this.row += 1;
+        }
+
+        //if daleks col is more than doctors col, -1 from daleks col
+        if (this.col > docCol) {
+            this.col -= 1;
+        
+        //if daleks col row is less than doctors col, +1 to daleks col
+        } else if (this.col < docCol) {
+            this.col += 1;
+
+        }
+
     }
-    
-    public int getCol(){
+
+    /**
+     * returns the current row of dalek
+     *
+     * @return the current row of dalek
+     */
+    public int getRow() {
+        return this.row;
+
+    }
+
+    /**
+     * return the current row of dalek
+     *
+     * @return the current row of dalek
+     */
+    public int getCol() {
         return this.col;
     }
-    
-    public int getRow(){
-        return this.row;
+
+    /**
+     * checks if dalek has crashed
+     *
+     * @return ture of false (
+     */
+    public boolean hasCrashed() {
+        return this.dCrash;
+
+    }
+
+    /**
+     * checks to see if daleks have collided
+     *
+     */
+    public void crash() {
+        this.dCrash = true;
+
     }
 }
